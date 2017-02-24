7fd465ff6de4283bc7daef8da523d0bc85f50cc3
himanoa <matsunoappy@gmail.com>
2017-02-24 12:27:41 +0900

    絵文字対応

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,21 +2,19 @@ require('redcarpet')
 require('gemoji')
 class Memo < ApplicationRecord
   belongs_to :user
+  def emojify(text)
+    text.gsub(/:([\w+-]+):/) do |match|
+      p $1
+      if emoji = Emoji.find_by_alias($1)
+        emoji.raw
+      else
+        match
+      end
+    end
+  end
   def rendered_body
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(filter_html: true),
                                           { autolink: true, strikethrough: true })
-    @rendered_body ||= emojinize(@markdown.render(body))
+    @rendered_body ||= self.emojify(@markdown.render(body))
   end
-
-  private
-    def emojinize(text)
-      text.gsub(/:([\w+-]+):/) do |match|
-        p $1
-        if emoji = Emoji.find_by_alias($1)
-          emoji.raw
-        else
-          match
-        end
-      end
-    end
 end
