22c2f394b090b7f3cde46c4dbb636db0fb0e805d
himanoa <matsunoappy@gmail.com>
2017-02-24 11:55:09 +0900

    :emoji:みたいな記法でemojiが書けるようになった

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,9 +1,22 @@
 require('redcarpet')
+require('gemoji')
 class Memo < ApplicationRecord
   belongs_to :user
   def rendered_body
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(filter_html: true),
                                           { autolink: true, strikethrough: true })
-    @rendered_body ||= @markdown.render(body)
+    @rendered_body ||= emojinize(@markdown.render(body))
   end
+
+  private
+    def emojinize(text)
+      text.gsub(/:([\w+-]+):/) do |match|
+        p $1
+        if emoji = Emoji.find_by_alias($1)
+          emoji.raw
+        else
+          match
+        end
+      end
+    end
 end
