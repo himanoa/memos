ec1dfe826a78ffbc3a29aa89a6673f7d2aa008eb
himanoa <matsunoappy@gmail.com>
2017-02-24 15:14:30 +0900

    hashtag and mention

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -13,9 +13,16 @@ class Memo < ApplicationRecord
       end
     end
   end
+  def twitter_hashtagnize(text)
+    hash_tag_reg = Regexp.new('#([Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー]+)')
+    text.gsub(hash_tag_reg) { |match| "<a href='https://twitter.com/hashtag/#{$1}'>#{match}</a>" }
+  end
+  def twitter_mentionize(text)
+    text.gsub(/@([a-zA-Z0-9_]{1,15})/){ |match| "<a href='https://twitter.com/#{$1}'>#{match}</a>" }
+  end
   def rendered_body
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(filter_html: true),
                                           { autolink: true, strikethrough: true })
-    @rendered_body ||= self.emojify(@markdown.render(body))
+    @rendered_body ||= twitter_mentionize(twitter_hashtagnize(emojify(@markdown.render(body))))
   end
 end
